30a71585d7a1ecb5a1c83e99e3aaccf279792478
maxim <maxim@webro.com.ua>
2016-08-23 23:27:59 +0300

    parent updates

--- parent.js
+++ parent.js
@@ -1,23 +1,20 @@
+
 var express = require('express');
 var app = express();
-
 var formidable = require('formidable');
 var util = require('util');
-
 const exec = require('child_process').exec;
-
+app.get('/', function(req, res){
+    res.end('I"m alive');
+});
 app.post('/crawl', function(req, res){
     var form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
-
     form.parse(req, function(err, fields, files) {
-
         res.writeHead(200, {'content-type': 'text/plain'});
         res.end('start crawl: ' + JSON.stringify(fields));
-
-        var maxBufferBytes = 1024 * 1024 * 1024;
-        var tiimeOutMiliseconds = 1000 * 60 * 10;
-
+        var maxBufferBytes = 1024 * 1024;
+        var tiimeOutMiliseconds = 1000 * 60 * 2;
         exec('node crawler.js ' + fields.url + ' ' + fields.elasticIp, [], {maxBuffer: maxBufferBytes, timeout: tiimeOutMiliseconds}, function(error, stdout, stderr) {
             if (error) {
                 console.error('exec error: ', error);
@@ -26,14 +23,10 @@ app.post('/crawl', function(req, res){
             console.log('stdout: ', stdout);
             console.log('stderr: ', stderr);
         });
-
     });
-
 });
-
-var server = app.listen(8000, function () {
+var server = app.listen(8124, function () {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('Example app listening at http://%s:%s', host, port)
 });
